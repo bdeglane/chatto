1d54600b2f2b929fa8eb7422316423736cacb6cd
Your Name <deglane.benoit@gmail.com>
2018-04-13 12:38:58 +0200

    add react animation on message appear

--- src/component/chatto-message/ChattoMessage.tsx
+++ src/component/chatto-message/ChattoMessage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { IChattoMessage } from '../../model/message';
 import * as cx from 'classnames';
+import { CSSTransitionGroup } from 'react-transition-group';
 import './ChattoMessage.css';
 
 export interface IChattoMessageProps {
@@ -23,7 +24,17 @@ export const ChattoMessage: React.SFC<IChattoMessageProps> = (props) => {
 
     const classNames = cx('message', {mine});
 
+    // with simple animation
+    // @link https://github.com/reactjs/react-transition-group/tree/v1-stable#animate-initial-mounting
     return (
-        <div className={classNames}>{message.text}</div>
+        // tslint:disable
+        <CSSTransitionGroup
+            transitionName='example'
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnter={false}
+            transitionLeave={false}>
+            <div className={classNames}>{message.text}</div>
+        </CSSTransitionGroup>
     );
 };
