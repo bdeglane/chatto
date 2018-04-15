a4f83abd58fb7f4ace4e9f8efc71607e136adc08
bdeglane <bdeglane@users.noreply.github.com>
2018-04-15 15:32:37 +0200

    disable lint on max length

--- src/component/chatto-message/chattoMessage.test.tsx
+++ src/component/chatto-message/chattoMessage.test.tsx
@@ -31,12 +31,14 @@ describe('ChattoInput', () => {
     it('should render my message', () => {
         const wrapper = shallow(<ChattoMessage message={props.message} mine={true}/>);
         const p = wrapper.find('div.message');
+        // tslint:disable:max-line-length
         assert.equal(p.html(), `<div class="message mine"><div class="user"></div><div class="text">${dummyMessage.text}</div></div>`);
     });
 
     it('should render another message', () => {
         const wrapper2 = shallow(<ChattoMessage message={props.message} mine={false}/>);
         const p2 = wrapper2.find('div.message');
+        // tslint:disable:max-line-length
         assert.equal(p2.html(), `<div class="message"><div class="user"><img class="chatto-pic" src="${dummyMessage.user.pic}" alt="${dummyMessage.user.name} profile picture"/></div><div class="text">${dummyMessage.text}</div></div>`);
     });
 });
