4100cf3aea96fa778297f9a6a6edb98600ad518a
bdeglane <bdeglane@users.noreply.github.com>
2018-04-15 16:11:47 +0200

    clean

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <App/>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
